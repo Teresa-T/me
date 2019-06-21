f56a47004628643b4955f75cc25042ca83004605
Teresa Tan <shaojun17700@gmail.com>
2019-06-21 13:43:42 +1000

    Week 4 Lab Exercise start

--- week4/exercise1.py
+++ week4/exercise1.py
@@ -36,7 +36,12 @@ def get_some_details():
     json_data = open(LOCAL + "/lazyduck.json").read()
 
     data = json.loads(json_data)
-    return {"lastName": None, "password": None, "postcodePlusID": None}
+    lastName = data["results"][0]["name"]["last"]
+    password = data["results"][0]["login"]["password"]
+    postcode = data["results"][0]["location"]["postcode"]
+    ID = data["results"][0]["id"]["value"]
+    postcodePlusID = postcode + int(ID)
+    return {"lastName": lastName, "password": password, "postcodePlusID": postcodePlusID}
 
 
 def wordy_pyramid():
